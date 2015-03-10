2876f1942838c64256e221494bea1830201b29c4
mcanouil <mickael.canouil@cnrs.fr>
2015-03-10 16:46:50 +0100

    Version 1.0.10

--- R/multiplot.R
+++ R/multiplot.R
@@ -1,8 +1,14 @@
-multiplot <- function (..., plotlist = NULL, cols = 1, rows = 1, layout = NULL) {
+multiplot <- function (..., plotlist = NULL, rows = 1, cols = 1, layout = NULL) {
     plots <- c(list(...), plotlist)
-    numPlots = length(plots)
+    numPlots <- length(plots)
+    if (numPlots>1 & rows == 1 & cols == 1) {
+        dimPlot <- n2mfrow(numPlots)
+        rows <- dimPlot[1]
+        cols <- dimPlot[2]
+    } else {}
     if (is.null(layout)) {
-        layout <- matrix(seq(1, cols * ceiling(numPlots/cols)), ncol = cols, nrow = rows, byrow = TRUE)
+        # layout <- matrix(seq(1, cols * ceiling(numPlots/cols)), ncol = cols, nrow = rows, byrow = TRUE)
+        layout <- matrix(seq(1, cols * ceiling(numPlots/cols)), nrow = rows, ncol = cols, byrow = TRUE)
     } else {}
     if (numPlots==1) {
         print(plots[[1]])
