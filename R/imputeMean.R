4437bbc3e0cdc8b977b0094a082d75a75c5eae84
mcanouil <mickael.canouil@cnrs.fr>
2015-10-02 15:00:46 +0200

    Version 1.0.14

--- R/imputeMean.R
+++
@@ -1,5 +0,0 @@
-imputeMean <- function (df) {
-    newDf <- as.data.frame(apply(df, 2, function (x) {ifelse(is.na(x), mean(x, na.rm = TRUE), x)}))
-    cat("Nombre de valeurs manquantes imputees : ", sum(apply(df, 2, is.na))-sum(apply(newDf, 2, is.na)), "/", sum(apply(df, 2, is.na)), " (", ((sum(apply(df, 2, is.na))-sum(apply(newDf, 2, is.na)))/sum(apply(df, 2, is.na)))*100, "%)", "\n", sep = "")
-    return(newDf)
-}
